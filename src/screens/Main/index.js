28dbcda09894950fe5c9b29f061318a27da92528
Filipe Macedo <fmacedoo@MacBook-Pro-de-user.local>
2019-05-09 16:03:16 -0300

    Added animation to the main Card element. Applied opacity to the menu and nav and also transform y position of nav element.

--- src/screens/Main/index.js
+++ src/screens/Main/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
+import { Animated } from 'react-native';
+import { PanGestureHandler } from 'react-native-gesture-handler';
+
 import { Container, Content, Card, CardHeader, CardContent, Title, Description, Annotation, CardFooter } from './styles';
 
 import Header from '../../components/Header';
@@ -8,33 +11,65 @@ import Tabs from '../../components/Tabs';
 import Menu from '../../components/Menu';
 
 export default function Main() {
+
+  const translateY = new Animated.Value(0);
+  
+  const animatedEvent = Animated.event(
+    [
+      {
+        nativeEvent: {
+          translationY: translateY,
+        }
+      }
+    ],
+    { useNativeDriver: true }
+  );
+
+  function onHandlerStateChange(event) {
+
+  }
+
   return (
     <Container>
       <Header />
 
       <Content>
 
-        <Menu />
+        <Menu translateY={translateY} />
 
-        <Card>
-          <CardHeader>
-            <Icon name="attach-money" size={28} color="#666" />
-            <Icon name="visibility-off" size={28} color="#666" />
-          </CardHeader>
-          <CardContent>
-            <Title>Account balance</Title>
-            <Description>$ 304,642.91</Description>
-          </CardContent>
-          <CardFooter>
-            <Annotation>
-              Transfer of $ 20.00 received from John Doe successfully today at 6PM.
-            </Annotation>
-          </CardFooter>
-        </Card>
-        
+        <PanGestureHandler
+          onGestureEvent={animatedEvent}
+          onHandlerStateChange={onHandlerStateChange}
+        >
+          <Card style={{
+            transform: [{
+              translateY: translateY.interpolate({
+                inputRange: [-350, 0, 380],
+                outputRange: [-50, 0, 380],
+                extrapolate: 'clamp',
+              }),
+            }],
+          }}
+          >
+            <CardHeader>
+              <Icon name="attach-money" size={28} color="#666" />
+              <Icon name="visibility-off" size={28} color="#666" />
+            </CardHeader>
+            <CardContent>
+              <Title>Account balance</Title>
+              <Description>$ 304,642.91</Description>
+            </CardContent>
+            <CardFooter>
+              <Annotation>
+                Transfer of $ 20.00 received from John Doe successfully today at 6PM.
+              </Annotation>
+            </CardFooter>
+          </Card>
+        </PanGestureHandler>
+
       </Content>
 
-      <Tabs />
+      <Tabs translateY={translateY} />
     </Container>
   );
 }
